ed7c0e5ea878922ac0ef3215d4ee14f201b2db6d
Yash Joshi <yashni3joshi@gmail.com>
2024-01-21 13:08:12 +0530

    Initial commit

---
+++ hacker-news/src/test/java/com/questionpro/test/HackerNewsApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questionpro.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerNewsApiIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
